1e8c1af2bfd5f657b09d6c4f33aecbe1b85296d0
dyu <david.yu.ftw@gmail.com>
2020-09-25 03:15:56 +0800

    default value for long fields

--- src/type.js
+++ src/type.js
@@ -211,7 +211,10 @@ Type.generateConstructor = function generateConstructor(mtype) {
             ("this%s={}", util.safeProp(field.name));
         else if (field.repeated) gen
             ("this%s=[]", util.safeProp(field.name));
-        else if (!field.bytes && (!oneofs || !isFieldOneOf(oneofs, keys, field))) gen
+        else if (field.bytes || (oneofs && isFieldOneOf(oneofs, keys, field))) /* noop */;
+        else if (field.long) gen
+            ("this%s=0", util.safeProp(field.name));
+        else gen
             ("this%s=%j", util.safeProp(field.name), field.typeDefault); // also messages (=null)
     return gen
     ("if(p)for(var ks=Object.keys(p),i=0;i<ks.length;++i)if(p[ks[i]]!=null)") // omit undefined or null
